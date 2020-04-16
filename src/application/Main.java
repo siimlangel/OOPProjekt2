7769fbb463e32999aef6017503ac896668b2044a
siimlangel <siimlangel11@hotmail.com>
2020-04-16 18:15:42 +0300

    bugfix

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,7 +7,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     // Teeb sisselogimise stage
     @Override
     public void start(Stage primaryStage) {
